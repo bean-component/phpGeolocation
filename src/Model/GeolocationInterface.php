80ae473e58813e4448da41690dd2255d24044b2a
binhle410 <binh@sunrise.vn>
2019-12-14 00:40:20 +0700

    - GeolocationInterface should extend ThingInterface - travis.yml and composer-travis.json as updated with phpThing

--- src/Model/GeolocationInterface.php
+++ src/Model/GeolocationInterface.php
@@ -2,56 +2,58 @@
 
 namespace Bean\Geolocation\Model;
 
+use Bean\Thing\Model\ThingInterface;
+
 /**
  * @author Binh Le <peterbean@bean-component.com>
  */
-interface GeolocationInterface
+interface GeolocationInterface extends ThingInterface
 {
     public function getAddress(): ?string;
 
-    public function setAddress(?string $address): self;
+    public function setAddress(?string $address): GeolocationInterface;
 
     public function getCountry(): ?string;
 
-    public function setCountry(?string $country): self;
+    public function setCountry(?string $country): GeolocationInterface;
 
     public function getFirstDivision(): ?string;
 
-    public function setFirstDivision(?string $firstDivision): self;
+    public function setFirstDivision(?string $firstDivision): GeolocationInterface;
 
     public function getSecondDivision(): ?string;
 
-    public function setSecondDivision(?string $secondDivision): self;
+    public function setSecondDivision(?string $secondDivision): GeolocationInterface;
 
     public function getThirdDivision(): ?string;
 
-    public function setThirdDivision(?string $thirdDivision): self;
+    public function setThirdDivision(?string $thirdDivision): GeolocationInterface;
 
     public function getFourthDivision(): ?string;
 
-    public function setFourthDivision(?string $fourthDivision): self;
+    public function setFourthDivision(?string $fourthDivision): GeolocationInterface;
 
     public function getFifthDivision(): ?string;
 
-    public function setFifthDivision(?string $fifthDivision): self;
+    public function setFifthDivision(?string $fifthDivision): GeolocationInterface;
 
     public function getStreet(): ?string;
 
-    public function setStreet(?string $street): self;
+    public function setStreet(?string $street): GeolocationInterface;
 
     public function getNumber(): ?string;
 
-    public function setNumber(?string $number): self;
+    public function setNumber(?string $number): GeolocationInterface;
 
     public function getLatitude(): ?float;
 
-    public function setLatitude(float $latitude): self;
+    public function setLatitude(float $latitude): GeolocationInterface;
 
     public function getLongitude(): ?float;
 
-    public function setLongitude(float $longitude): self;
+    public function setLongitude(float $longitude): GeolocationInterface;
 
     public function getGooglePlaceId(): ?string;
 
-    public function setGooglePlaceId(?string $placeId): self;
+    public function setGooglePlaceId(?string $placeId): GeolocationInterface;
 }
